e5d6d17140b755999a8ba8b0c6ae788b111e4c19
HawkManHawkPlan <60548332+HawkManHawkPlan@users.noreply.github.com>
2021-09-27 17:39:03 -0400

    Animation Branch Commit

--- Assets/Scripts/AudioPlay.cs
+++ Assets/Scripts/AudioPlay.cs
@@ -6,31 +6,33 @@ public class AudioPlay : MonoBehaviour
 {
 	public AudioClip clip1, clip2;
     public AudioSource musicSource;
+    public Animator anim;
 	private void Update()
 	{
         if (Input.GetKeyDown(KeyCode.W))
         {
             musicSource.clip = clip1;
             musicSource.Play();
-
+            anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
-
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = clip2;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
-
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.L))
